b838440c6638df7344832d3c2dcf11d3f91bee69
zaadevofc <zaadevofc@gmail.com>
2023-12-05 20:20:00 +0700

    finish✨✨

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
+import vercel from "@astrojs/vercel/serverless";
+
 export default defineConfig({
   integrations: [tailwind()],
-  vite: {
-    server: {
-      hmr: {
-        protocol: 'ws'
-      }
-    },
-    apiPrefix: '/api' 
-  }
+  output: 'server',
+  adapter: vercel()
 });
